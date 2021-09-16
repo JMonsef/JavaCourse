90aa57f3dd7edf8c55bcbeadbea34caa7b45cb51
JMonsef <monsefjwahri@gmail.com>
2021-09-16 15:54:29 +0200

    cambios

--- CursoJavaManPower/src/modulo1/Ejercicio1.java
+++ CursoJavaManPower/src/modulo1/Ejercicio1.java
@@ -5,6 +5,8 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		//TODO Hello Gabriel
 		System.out.println("gggggg");
+		System.out.println("\n\n\n con esto consigues saltos de linea");
+		System.out.println("con esto tabulas \t\t dentro de la linea");
 	}
 
 }
