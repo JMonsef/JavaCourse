8d26131883be0ca8e92c2c9efabad1f8e6c5c481
JMonsef <monsefjwahri@gmail.com>
2021-09-16 16:31:34 +0200

    ejercicios de clases

---
+++ CursoJavaManPower/src/modulo1/Ejercicio2.java
@@ -0,0 +1,19 @@
+package modulo1;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		/*
+		 *1- Realizar un programa identificado como Ejercicio2.java 
+		 * 	(nos , en el cual me permita mostrar el 
+		 *	nombre de los compañeros de grado de la siguiente forma)
+		 *
+		 */
+		System.out.println("Nombre \t\t Apellido \t\t email \t\t teléfono");
+		System.out.println("------ \t\t -------- \t\t ----- \t\t --------");
+		System.out.println("Monsef \t\t Bakhtaoui \t\t monsef@monsef.es 66666666");
+	}
+
+}
---
+++ CursoJavaManPower/src/modulo1/Ejercicio3.java
@@ -0,0 +1,19 @@
+package modulo1;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		/*
+		 * 2- Realizar un programa identificado como Ejercicio3.java 
+		 * que permita escribir el siguiente texto
+		 */
+		
+		System.out.println("Tecla de escape \t\t Significado \n");
+		System.out.println("\\n \t\t\t\t significa nueva línea");
+		System.out.println("\\t \t\t\t\t significa un tab de espacio");
+		System.out.println("\\\" \t\t\t\t es para poner \" (comillas dobles) dentro del texto por ejemplo \"Belencita\"");
+		System.out.println("\\\\ \t\t\t\t se utiliza para escribir la \\ dentro del texto, por ejemplo \\algo\\");
+		System.out.println("\\\' \t\t\t\t se utiliza para las \' (comilla simple) para escribir por ejemplo \'Princesita\'");
+	}
+
+}
