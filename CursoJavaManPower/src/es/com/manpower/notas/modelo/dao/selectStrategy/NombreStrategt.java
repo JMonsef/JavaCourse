5bc40d1ec7b98757be08e6a31574ad2c94ede203
JMonsef <monsefjwahri@gmail.com>
2021-09-24 14:13:03 +0200

    patron strategy hecho en clase

---
+++ CursoJavaManPower/src/es/com/manpower/notas/modelo/dao/selectStrategy/NombreStrategt.java
@@ -0,0 +1,21 @@
+package es.com.manpower.notas.modelo.dao.selectStrategy;
+
+public class NombreStrategt extends SelectStrategy {
+
+	public NombreStrategt() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getCondicion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isMe() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
