9277f6fd22de4234c99a219dc0674375086c4f0f
JMonsef <monsefjwahri@gmail.com>
2021-09-27 19:37:40 +0200

    algo falla, no va pipi cucu

---
+++ spring-workspace/HolaSpringFrameWork/src/main/java/com/domain/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.domain.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/home")
+	public String goIndex() {
+		System.out.println("pase por aqui");
+		return "Index";
+	}
+
+}
